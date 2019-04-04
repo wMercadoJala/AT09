726d1a70f221622952678134c1e68ffd4385fc35
Raul Choque <Raul.Choque@fundacion-jala.org>
2019-04-04 14:10:38 -0400

    First practice about use operators was complete

---
+++ practices/raul/python1_practice1.py
@@ -0,0 +1,15 @@
+value_of_first_number = 5
+value_of_second_number = 2
+result_of_adds_value_of_first_number_and_value_of_second_number = value_of_first_number + value_of_second_number
+print("Result of addition two values: ", value_of_first_number, " + ", value_of_second_number, " = ", result_of_adds_value_of_first_number_and_value_of_second_number)
+result_of_subtract_value_of_first_number_and_value_of_second_number = value_of_first_number - value_of_second_number
+print("Result of subtraction between two values: ", value_of_first_number, " - ", value_of_second_number, " = ", result_of_subtract_value_of_first_number_and_value_of_second_number)
+result_of_multiplies_value_of_first_number_and_value_of_second_number = value_of_first_number * value_of_second_number
+print("Result of multiplication two values: ", value_of_first_number, " * ", value_of_second_number, " = ", result_of_multiplies_value_of_first_number_and_value_of_second_number)
+result_of_divides_value_of_first_number_and_value_of_second_number = value_of_first_number // value_of_second_number
+print("Result of division two values: ", value_of_first_number, " / ", value_of_second_number, " = ", result_of_divides_value_of_first_number_and_value_of_second_number)
+result_of_modulus_value_of_first_number_and_value_of_second_number = value_of_first_number % value_of_second_number
+print("Result of modulus between two values: ", value_of_first_number, " % ", value_of_second_number, " = ", result_of_modulus_value_of_first_number_and_value_of_second_number)
+result_of_exponent_value_of_first_number_and_value_of_second_number = value_of_first_number ** value_of_second_number
+print("Result of exponent two values: ", value_of_first_number, " ** ", value_of_second_number, " = ", result_of_exponent_value_of_first_number_and_value_of_second_number)
+
