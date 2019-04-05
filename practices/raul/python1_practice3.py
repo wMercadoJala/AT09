cae8d4c59addc146d67e509a546c977594722274
Raul Choque <Raul.Choque@fundacion-jala.org>
2019-04-05 16:03:12 -0400

    practice eighth about counter of letter in a text was complete

--- practices/raul/python1_practice3.py
+++ practices/raul/python1_practice3.py
@@ -7,10 +7,33 @@ print("This is the url: ", url)
 
 
 #PRACTICE 07
-def replace(text, old_value, new_value):
+def replace_word(text, old_value, new_value):
     print(text.replace(old_value, new_value))
 
 
-replace("Mississippi", "i", "I")
-replace("I love spom! Spom is my favorite food.Spom, spom, yum!", "om", "am")
-replace("I love spom! Spom is my favorite food.Spom, spom, yum!", "o", "a")
+replace_word("Mississippi", "i", "I")
+replace_word("I love spom! Spom is my favorite food.Spom, spom, yum!", "om", "am")
+replace_word("I love spom! Spom is my favorite food.Spom, spom, yum!", "o", "a")
+
+
+#PRACTICE 08
+def counter_letter_in_text(text):
+    text_without_space  = text.replace(" ", "")
+    print(text_without_space)
+    cont = 0
+    dic = {}
+    for character in text_without_space:
+        char_lower = character.lower()
+        for charactertwo in text_without_space:
+            if (char_lower == charactertwo.lower()) and (character != " "):
+                cont += 1
+
+        dic[char_lower] = cont
+        cont = 0
+    for i in sorted(dic):
+        print((i, dic[i]))
+
+
+counter_letter_in_text("ThiS is String with Upper and lower case Letters")
+
+
