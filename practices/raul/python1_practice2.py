ba52075c2d57a665fcb7c19d3d81e5056b90c56b
raulchoque <yfert_0209@hotmail.com>
2019-04-05 00:05:55 -0400

    practice fourth about sum all integer numbers until 35 was complete

--- practices/raul/python1_practice2.py
+++ practices/raul/python1_practice2.py
@@ -21,7 +21,29 @@ def is_prime_number(param):
     return is_prime
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-for i in range(len(numbers)):
+for i in range(3, len(numbers)):
     if is_prime_number(numbers[i]):
         print("Number: ", numbers[i], " is prime.")
     else: print("Number: ", numbers[i], " isn't prime.")
+
+#PRACTICE 04.1
+def area_of_circle(radio):
+    if radio > 10:
+        pi = 3.1415
+        area = pi * (radio ** 2)
+        print("Area is equal: ", area)
+    else: print("the value of ratio have to greater of 10")
+
+
+area_of_circle(11)
+
+#PRACTICE 04.2
+def sum_to(until_number):
+    sum = 0
+    addition = 1
+    max_number = 35
+    while (addition <= until_number) and (addition <= max_number):
+        sum += addition
+        addition += 1
+    print(sum)
+sum_to(50)
