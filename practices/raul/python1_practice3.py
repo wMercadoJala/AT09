a66bd6a59a6ec632e3e8ebd0f2891fca59f1b15f
Raul Choque <Raul.Choque@fundacion-jala.org>
2019-04-05 14:35:13 -0400

    practice seventh about replace a letter or letters in a text was complete

--- practices/raul/python1_practice3.py
+++ practices/raul/python1_practice3.py
@@ -4,3 +4,13 @@ text_with_init_url = text[text.find("https://"): len(text)]
 post_end_url = text_with_init_url.find(" ")
 url = text_with_init_url[0:post_end_url]
 print("This is the url: ", url)
+
+
+#PRACTICE 07
+def replace(text, old_value, new_value):
+    print(text.replace(old_value, new_value))
+
+
+replace("Mississippi", "i", "I")
+replace("I love spom! Spom is my favorite food.Spom, spom, yum!", "om", "am")
+replace("I love spom! Spom is my favorite food.Spom, spom, yum!", "o", "a")
