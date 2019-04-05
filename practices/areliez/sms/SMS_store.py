403471977724b63f5d5260af28beedf8a6028ded
Areliez Vargas <areliez.vargas@gmail.com>
2019-04-05 16:12:08 -0400

    Add functions to add_message and count_messages

--- practices/areliez/sms/SMS_store.py
+++ practices/areliez/sms/SMS_store.py
@@ -1,13 +1,27 @@
+import datetime
+import re
+
+
 class SMS_store:
 
     def __init__(self):
         self.messages = []
 
-    def add_new_arrival(self, from_number, time_arrived, text_of_SMS):
-        pass
+    def add_new_arrival(self, from_number, text_of_SMS):
+        message = []
+        time_arrived = datetime.datetime.now().strftime('%H:%M:%S')
+        message.append(time_arrived)
+        number_phone = from_number
+        message.append(number_phone)
+        text = text_of_SMS
+        message.append(text)
+        self.messages.append(message)
 
     def message_count(self):
-        pass
+        count = 0
+        for message in self.messages:
+            count += 1
+        return count
 
     def get_unread_indexes(self):
         pass
@@ -20,3 +34,11 @@ class SMS_store:
 
     def clear(self):
         pass
+
+
+my_store = SMS_store()
+my_store.add_new_arrival("70376674", "hola mundo")
+my_store.add_new_arrival("70376674", "hola kathy")
+
+print(my_store.messages)
+print(my_store.message_count())
