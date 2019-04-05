1b1cfa0e5a75bd26c0272a764880eedaa2d03609
Areliez Vargas <areliez.vargas@gmail.com>
2019-04-05 12:33:28 -0400

    Create the class SMS_ store and it functions

---
+++ practices/areliez/sms/SMS_store.py
@@ -0,0 +1,22 @@
+class SMS_store:
+
+    def __init__(self):
+        self.messages = []
+
+    def add_new_arrival(self, from_number, time_arrived, text_of_SMS):
+        pass
+
+    def message_count(self):
+        pass
+
+    def get_unread_indexes(self):
+        pass
+
+    def get_message(self, i):
+        pass
+
+    def delete(self, i):
+        pass
+
+    def clear(self):
+        pass
