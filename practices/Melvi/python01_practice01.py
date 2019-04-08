3ccd2404f0460b7501609c0f0b166ff86a74c595
melvicaballero <melvi.caballero@gmail.com>
2019-04-08 11:17:22 -0400

    Practice01 Add more katas.

--- practices/Melvi/python01_practice01.py
+++ practices/Melvi/python01_practice01.py
@@ -96,17 +96,193 @@ def no_space(string):
 
     return texto
 
-print (no_space())
+print (no_space("H o l a ."))
 
 '''Write a program that reads a string and returns a table of the letters of the alphabet
- in alphabetical order which occur in the string together with the number of times each letter occurs. ​
+in alphabetical order which occur in the string together with the number of times each letter occurs. ​
 Case should be ignored. A sample output of the program when the user enters the data ​
 “ThiS is String with Upper and lower case Letters”,
 '''
-string = 'ThiS is String with Upper and lower case Letters'
-letras = ['a','c','d','e','g','h','i','l','o','p','r','s','t','w','u']
+input_text = 'ThiS is String with Upper and lower case Letters'
+lista = ['a','c','d','e','g','h','i','l','o','p','r','s','t','w','u']
 dic = {}
-for i in letras:
-   dic.update({i:(string.count(i))})
+for i in lista:
+   dic.update({i:(input_text.count(i))})
 
 print (dic)
+
+#Refactor
+'''Write a program that reads a string and returns a table of the letters of the alphabet
+in alphabetical order which occur in the string together with the number of times each letter occurs. ​
+Case should be ignored. A sample output of the program when the user enters the data ​
+“ThiS is String with Upper and lower case Letters”,
+'''
+
+
+def ordered_count(input_text):
+    lista = []
+    tam = len(input_text)
+    text = input_text
+    while tam > 0:
+        lista.append((text[0], input_text.count(text[0])))
+        text = text.replace(text[0], "")
+        tam = len(text)
+
+    return lista
+
+#test
+print (ordered_count("ThiS is String with Upper and lower case Letters"))
+
+'''Your task is to remove all duplicate words from string, leaving only single (first) words entries.
+Example:
+Input:
+'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+Output:
+'alpha beta gamma delta' '''
+
+def remove_duplicate_words(text):
+    lista = ""
+    newtext = text.split()
+    while newtext:
+        lista = lista + newtext[0] + " "
+        text = text.replace(newtext[0], "")
+        newtext = text.split()
+
+    lista = lista.strip()
+    return lista
+#Test
+print (remove_duplicate_words('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'))
+
+'''A Rule of Divisibility by 7
+A number m of the form 10x + y is divisible by 7 if and only if x − 2y is divisible by 7. In other words, subtract twice the last digit from the number formed by the remaining digits. Continue to do this until a number known to be divisible or not by 7 is obtained; you can stop when this number has at most 2 digits because you are supposed to know if a number of at most 2 digits is divisible by 7 or not.
+The original number is divisible by 7 if and only if the last number obtained using this procedure is divisible by 7.
+Examples:
+1 - m = 371 -> 37 − (2×1) -> 37 − 2 = 35 ; thus, since 35 is divisible by 7, 371 is divisible by 7.
+The number of steps to get the result is 1.
+2 - m = 1603 -> 160 - (2 x 3) -> 154 -> 15 - 8 = 7 and 7 is divisible by 7.'''
+
+def seven(number):
+    cont = 0
+    while number > 99:
+        a = number % 10
+        b = number // 10
+        number = b-(a*2)
+        cont += 1
+
+    return tuple((number,cont) )
+#Test
+print (seven(1063))
+
+''' FizzBuzz
+Return an array containing the numbers from 1 to N, where N is the parametered value.
+N will never be less than 1 (in C#, N might be less then 1).
+C# ONLY: If N is smaller then or equal to 0, throw an ArgumentOutOfRangeException!
+Replace certain values however if any of the following conditions are met:
+If the value is a multiple of 3: use the value 'Fizz' instead
+If the value is a multiple of 5: use the value 'Buzz' instead
+If the value is a multiple of 3 & 5: use the value 'FizzBuzz' instead'''
+
+
+def fizzbuzz(number):
+    lista = []
+    for i in range(1, number + 1):
+        if i % 3 == 0:
+            if i % 5 == 0:
+                lista.append("FizzBuzz")
+            else:
+                lista.append("Fizz")
+        else:
+            if i % 5 == 0:
+                lista.append("Buzz")
+            else:
+                lista.append(i)
+
+    return lista
+#Test
+print (fizzbuzz(30))
+
+'''Reverse words
+Complete the function that accepts a string parameter, and reverses each word in the string.
+All spaces in the string should be retained.
+Examples:
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+'''
+import re
+def reverse_words(text):
+    newstr = re.split(r'(\s+)', text)
+    result = ""
+    for j in newstr:
+        new = ""
+        for i in range(len(j) - 1, -1, -1):
+            new = new + j[i]
+        result += new
+
+    return result
+
+#Test
+print (reverse_words("double  spaces"))
+
+'''Sum Factorial
+Factorials are often used in probability and are used as an introductory problem for looping constructs.
+In this kata you will be summing together multiple factorials.
+Here are a few examples of factorials:
+4 Factorial = 4! = 4 * 3 * 2 * 1 = 24
+6 Factorial = 6! = 6 * 5 * 4 * 3 * 2 * 1 = 720
+In this kata you will be given a list of values that you must first find the factorial, and then return their sum.
+For example if you are passed the list [4, 6] the equivalent mathematical expression would be 4! + 6! which would equal 744.
+'''
+
+def factorial(num):
+    if num == 1:
+        return 1
+    else:
+        return num * factorial(num - 1)
+
+def sum_factorial(lst):
+    suma = 0
+    for i in lst:
+        suma = suma + factorial(i)
+
+    return suma
+#Test
+print (sum_factorial([4,6]))
+
+'''Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence
+Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+Examples
+replace("Hi!") === "H!!"
+replace("!Hi! Hi!") === "!H!! H!!"
+replace("aeiou") === "!!!!!"
+replace("ABCDE") === "!BCD!"
+'''
+def replace_exclamation(text):
+    list=["a","e","i","o","u","A","E","I","O","U"]
+    for i in list:
+        text=text.replace(i,"!")
+    return text
+
+#Test
+print (replace_exclamation("ABCDE"))
+
+'''Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right
+Remove n exclamation marks in the sentence from left to right. n is positive integer.
+Examples
+remove("Hi!",1) === "Hi"
+remove("Hi!",100) === "Hi"
+remove("Hi!!!",1) === "Hi!!"
+remove("Hi!!!",100) === "Hi"
+remove("!Hi",1) === "Hi"
+remove("!Hi!",1) === "Hi!"
+remove("!Hi!",100) === "Hi"
+remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"'''
+def remove(text, number):
+    text=text.replace("!","",number)
+    return text
+
+#test
+print (remove("!!!Hi !!hi!!! !hi",1))
+print (remove("!!!Hi !!hi!!! !hi",5))
